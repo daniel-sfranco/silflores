aad24b9c69cfa3c7615e300087e681fc47330790
daniel-sfranco <daniel.sfranco346@outlook.com>
2024-09-13 12:10:10 -0300

    Setup finishing and first views

--- silfloresapp/silflores/settings.py
+++ silfloresapp/silflores/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'silflores.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- silfloresapp/silflores/urls.py
+++ silfloresapp/silflores/urls.py
@@ -18,9 +18,12 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home),
+    path('about/', views.about)
 ]
 
 if settings.DEBUG:
