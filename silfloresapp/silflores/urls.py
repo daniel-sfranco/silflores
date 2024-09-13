aa4f999f20ffca0ecd673e98b395502ec3d7d13f
daniel-sfranco <daniel.sfranco346@outlook.com>
2024-09-13 13:43:31 -0300

    Creating Products app and checking static files

--- silfloresapp/silflores/urls.py
+++ silfloresapp/silflores/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
@@ -23,7 +23,8 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home),
-    path('about/', views.about)
+    path('about/', views.about),
+    path('products/', include('products.urls'))
 ]
 
 if settings.DEBUG:
